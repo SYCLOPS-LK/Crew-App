0c7506da433d1b1a6dccb813415360a50faa03d9
Lakshitha Prabhath <SYCLOPS-LK>
2022-04-06 07:35:25 +0530

    Screens Seperated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crew_app/model/user_model.dart';
+import 'package:crew_app/screens/crew_screen.dart';
 import 'package:crew_app/screens/login_screen.dart';
+import 'package:crew_app/screens/settings_screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -27,34 +30,44 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  final bottomNavigationBarItems = <Widget>[
+    Icon(
+      Icons.person_outline,
+      size: 30.0,
+      color: Color.fromARGB(255, 20, 28, 36),
+    ),
+    Icon(
+      Icons.home_outlined,
+      size: 30.0,
+      color: Color.fromARGB(255, 20, 28, 36),
+    ),
+    Icon(
+      Icons.settings_outlined,
+      size: 30.0,
+      color: Color.fromARGB(255, 20, 28, 36),
+    ),
+  ];
+  var index = 1;
+
+  final screens = <Widget>[
+    CrewScreen(),
+    HomeScreen(),
+    SettingsScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       backgroundColor: const Color.fromARGB(255, 20, 28, 36),
-      bottomNavigationBar: BottomAppBar(
-        color: const Color.fromARGB(255, 246, 170, 79),
-        child: Row(
-          children: [
-            IconButton(
-              onPressed: () {
-                logout(context);
-              },
-              icon: const Icon(
-                Icons.exit_to_app_outlined,
-                color: Color.fromARGB(255, 20, 28, 36),
-              ),
-            ),
-            Text(
-              "${loggedInUser.firstName} | ${loggedInUser.email}".toUpperCase(),
-              textAlign: TextAlign.end,
-              style: const TextStyle(
-                fontSize: 13.0,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 20, 28, 36),
-              ),
-            ),
-          ],
-        ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: index,
+        color: Color.fromARGB(255, 246, 170, 79),
+        backgroundColor: Colors.transparent,
+        height: 50.0,
+        items: bottomNavigationBarItems,
+        onTap: (index) => setState(() => this.index = index),
+        animationDuration: Duration(milliseconds: 200),
       ),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 246, 170, 79),
@@ -67,45 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.width / 2.4,
-                      width: MediaQuery.of(context).size.width / 2.4,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(25)),
-                        image: DecorationImage(
-                          image: AssetImage("assets/malith.jpg"),
-                          fit: BoxFit.contain,
-                          opacity: 30.0,
-                        ),
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 20, 28, 36),
-                            Color.fromARGB(255, 20, 28, 36),
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: screens[index],
     );
   }
 
